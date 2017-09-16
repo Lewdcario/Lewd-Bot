e58cf372a33977bc74b4f9ee8d885f1d79ef346d
Isabella <isabellafj97@gmail.com>
2017-09-15 22:39:08 -0600

    made it slightly less ugly i think

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### How to run this great bot
 * Insert token in `src.config.txt`
-* Launch Bot via `src.main.Window`
+* Launch Bot via `src.main.life`
 * Press **LOGIN**
 * Press **LOG THE FUCK OUT** to log out
--- src/main/Window.java
+++ src/main/Window.java
@@ -1,30 +1,83 @@
 package main;
 
 import net.dv8tion.jda.core.AccountType;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 import structures.LewdBot;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window {
 	private static LewdBot session = null;
-	private JPanel Frame;
+	private JPanel Main;
 	private JButton LOGINButton;
-	private JButton LOGTHEFUCKOUTButton;
-	private JLabel lewdBotLabel;
+	private JButton GTFOButton;
+	private JPasswordField passwordField1;
+	private JPanel background;
+	private JTextPane TOKENBITCHTextPane;
+	private static int width;
+	private static int height;
+
+	private void createUIComponents() {
+		try {
+			background = new JPanelWithBackground("./src/assets/bg.png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private Window() {
+		createUIComponents();
 		LOGINButton.addActionListener(e -> {
 			if (Window.session != null) System.err.println("Already logged in!");
 			else Window.session = new LewdBot(AccountType.BOT);
 		});
-		LOGTHEFUCKOUTButton.addActionListener(e -> System.exit(0));
+		GTFOButton.addActionListener(e -> System.exit(0));
+	}
+
+	private static void init() {
+		Window window = new Window();
+		window.Main.setPreferredSize(new Dimension(Window.width, Window.height));
+		setAppearance();
+		JFrame frame = new JFrame("Lewd Bot");
+		frame.setIconImage(new ImageIcon("./src/assets/icon.png").getImage());
+		frame.setContentPane(window.Main);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static class JPanelWithBackground extends JPanel {
+		private Image backgroundImage;
+		JPanelWithBackground(String fileName) throws IOException {
+			backgroundImage = ImageIO.read(new File(fileName));
+			Window.width = backgroundImage.getWidth(this);
+			Window.height = backgroundImage.getHeight(this);
+		}
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(backgroundImage, 0, 0, this);
+		}
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Window");
-		frame.setContentPane(new Window().Frame);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
+		init();
+	}
+
+	private static void setAppearance() {
+		try {
+			for (UIManager.LookAndFeelInfo info: UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		}
+		catch (Exception e) {
+			// If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 	}
 }
